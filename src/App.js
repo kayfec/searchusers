48aee9147658189ac5954234ea0caa9be0d5046b
Vladislav Kayhorodov <vladislav.kaihorodov@gmail.com>
2019-03-13 16:19:28 +0200

    Github search top 10 users

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users:[],
+      URL : 'https://api.github.com/search/users?q=location%3Acherkassy&sort=followers'
+    };
+  }
+  componentDidMount() {
+    this.search();
+  }
+
+  search = () => {
+
+    const getItems = async () => {
+      const result = await fetch('https://api.github.com/search/users?q=location%3Acherkassy&sort=followers');
+      const data = await result.json();
+      const items =  data.items.slice(0,10);
+      return items
+    }
+    return getItems().then(data => {
+      this.setState({
+        users:data
+      })
+    })
+  }
+
+  simlulateError = () => {
+    this.setState({
+      users:[]
+    })
+    throw new Error('wwww')
+  }
+
   render() {
+    setTimeout(() => {
+      if(this.state.users.length === 0) {
+        throw new Error('/wtf')
+      }
+
+    },5000)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <div>URL : <input defaultValue={this.state.URL} /></div>
+          <button>Search</button>
+          <button type='button' onClick={this.simlulateError}>error</button>
+          <ul>
+            {
+              this.state.users.map(item => {
+                const {id,avatar_url,login,html_url} = item;
+                return (
+                    <li key={id}>
+                      <img src={avatar_url} alt="person_img"/>
+                      <div><a href={html_url}>{login}</a></div>
+                    </li>
+                )
+              })
+            }
+          </ul>
+        </div>
     );
+
   }
 }
 
